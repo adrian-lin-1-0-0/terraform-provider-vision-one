b16ed71e69dc3ba2a8d11ba252f9c214febd0e92
adrian-lin-1-0-0 <adrian-lin-1-0-0@gmail.com>
2024-08-17 12:21:20 +0800

    Refactor provider initialization to align with Go idioms
    - Updated the  function to return a  directly, simplifying the function signature in line with Go's conventions.
    - Introduced a new  function to provide a factory method, preserving the previous behavior where a provider factory is needed.

--- main.go
+++ main.go
@@ -42,7 +42,7 @@ func main() {
 		Debug:   debug,
 	}
 
-	err := providerserver.Serve(context.Background(), provider.New(version), opts)
+	err := providerserver.Serve(context.Background(), provider.NewFactory(version), opts)
 
 	if err != nil {
 		log.Fatal(err.Error())
